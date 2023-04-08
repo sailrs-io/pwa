8b2309b3741c4cae98e2a982a2eba3afbb4c1d9b
Sebastian Kriems <sebastian.kriems@sailrs.io>
2023-04-08 02:43:29 +0200

    feat(notifications): provide default event handlers

--- src/lib/browser/pushNotifications.ts
+++ src/lib/browser/pushNotifications.ts
@@ -1,12 +1,45 @@
 /// <reference lib="webworker" />
 
 type PushNotificationsArgs = {
+  /** */
   onPush?: (event: PushEvent) => Promise<void>
+  /** */
+  onNotificationClick?: (event: NotificationEvent) => Promise<void>
 }
 
-export function pushNotifications({ onPush }: PushNotificationsArgs = {}) {
+function handlePush(event: PushEvent) {
+  return new Promise<void>((resolve) => {
+    if (event.data) {
+      const { title, ...options } = event.data.json();
+      (self as unknown as ServiceWorkerGlobalScope).registration.showNotification(title, {
+        ...options,
+      });
+    }
+    resolve()
+  })
+}
+
+function handleClick(event: NotificationEvent) {
+  return new Promise<void>((resolve) => {
+    if (!event.action) {
+      resolve()
+    }
+    // @ts-ignore
+    clients.openWindow(event.action)
+    resolve()
+  })
+}
+
+export function pushNotifications({ onPush, onNotificationClick }: PushNotificationsArgs = {}) {
   if (onPush) {
+    const handle = typeof onPush === 'function' ? onPush : handlePush;
     (self as unknown as ServiceWorkerGlobalScope)
-      .addEventListener('push', (event) => event.waitUntil(onPush(event)))
+      .addEventListener('push', (event) => event.waitUntil(handle(event)))
+  }
+
+  if (onNotificationClick) {
+    const handle = typeof onNotificationClick === 'function' ? onNotificationClick : handleClick;
+    (self as unknown as ServiceWorkerGlobalScope)
+      .addEventListener('notificationclick', (event) => event.waitUntil(handle(event)))
   }
 }
