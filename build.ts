700a1a07efcb147de1e654c4d7ca3c82c6933824
Sebastian Kriems <sebastian.kriems@sailrs.io>
2023-04-20 09:04:17 +0200

    chore: minify on prod build

--- build.ts
+++ build.ts
@@ -1,35 +1,30 @@
-import * as esbuild from 'esbuild';
-import { globSync } from 'glob';
+import * as esbuild from "esbuild";
+import { globSync } from "glob";
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
-const target = ['ES2019', 'node14.17'];
-console.log('isProduction', isProduction);
-
-const subModules = ['notifications'];
+const target = ["ES2019", "node14.17"];
+console.log("isProduction", isProduction);
 
 esbuild.buildSync({
-  entryPoints: globSync('src/**/*.ts'),
-  format: 'cjs',
-  outdir: 'dist/cjs',
+  entryPoints: globSync("src/**/*.ts"),
+  format: "cjs",
+  outdir: "dist/cjs",
   bundle: false,
   minify: isProduction,
   sourcemap: !isProduction,
-  platform: 'node',
+  platform: "node",
   target,
 });
 
 esbuild.buildSync({
-  entryPoints: [
-    'src/index.ts',
-    ...subModules.map((module) => `src/${module}/index.ts`),
-  ],
-  format: 'esm',
-  outdir: 'dist/esm',
-  bundle: true,
+  entryPoints: globSync("src/**/*.ts"),
+  format: "esm",
+  outdir: "dist/esm",
+  bundle: false,
   minify: isProduction,
   sourcemap: !isProduction,
   splitting: true,
-  outExtension: { '.js': '.mjs' },
+  outExtension: { ".js": ".mjs" },
   target,
 });
